df25d52ef34f73d4c5b8722f6228e614c45896da
jairomelo <jairom@colmich.edu.mx>
2018-05-31 13:27:47 -0500

    fixed last commit

--- convertir_y_backup.cmd
+++ convertir_y_backup.cmd
@@ -1,14 +1,21 @@
-REM crear copia de suguridad
+REM crear copia de seguridad archivos
 
 robocopy "C:\Users\jairomelo\Box Sync\ColMIch\Proyecto doctoral\TESIS\Capitulado" "C:\Users\jairomelo\Box Sync\ColMIch\Proyecto doctoral\TESIS_backup\Capitulado"
+
+REM copia de seguridad de subcarpetas
+
+robocopy "C:\Users\jairomelo\Box Sync\ColMIch\Proyecto doctoral\TESIS\Capitulado\imgs" "C:\Users\jairomelo\Box Sync\ColMIch\Proyecto doctoralTESIS_backup\Capitulado\imgs"
+
+REM it works?
+pause
 
 REM convertir los archivos de docx a markdown
 
 pandoc -s cap_1.docx -t markdown -o cap1.md
 pandoc -s archivos_y_fuentes.docx -t markdown -o archivos_y_fuentes.md
 
-REM iniciar GIT [por alguna razón no funciona :(
+REM iniciar GIT [it doesn't works for some reason :(]
 
-start git.exe
+start "C:\Program Files\Git\git-bash.exe"
 
 pause
