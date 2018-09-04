281d79f30576b054298bb88b066ddc8efb97b524
jairo melo <jairom@colmich.edu.mx>
2018-09-04 14:51:43 -0500

    mod -cmd

--- convertir_y_backup.cmd
+++ convertir_y_backup.cmd
@@ -1,3 +1,13 @@
+REM convertir los archivos de docx a markdown
+
+pandoc -s cap_1.docx -t markdown -o cap1.md
+pandoc -s cap_1.docx -t markdown -o cap1.md
+pandoc -s cap_2.docx -t markdown -o cap2.md
+pandoc -s archivos_y_fuentes.docx -t markdown -o archivos_y_fuentes.md
+pandoc -s cap_3_sync.docx -t markdown -o cap3.md
+pandoc -s borrador.docx -t markdown -o borrador.md
+
+
 REM crear copia de seguridad archivos
 
 robocopy 'ruta original' 'ruta del backup'
@@ -5,12 +15,6 @@ robocopy 'ruta original' 'ruta del backup'
 REM copia de seguridad de subcarpetas.
 
 robocopy 'ruta original' 'ruta del backup'
-
-REM convertir los archivos de docx a markdown
-
-pandoc -s cap_1.docx -t markdown -o cap1.md
-pandoc -s cap_2.docx -t markdown -o cap2.md
-pandoc -s archivos_y_fuentes.docx -t markdown -o archivos_y_fuentes.md
 
 REM iniciar GIT [abre git-bash desde CMD]
 
